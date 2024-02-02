2c92ee64e1749471e6e3917b67028d272396c787
Ryan Duran <dogoathena@gmail.com>
2024-02-02 08:57:57 -0800

    5th

--- list.js
+++ list.js
@@ -1,5 +1,12 @@
 function arrayToList(arr) {
-  
+//   for(k = 0; k < arr.length; k++) {
+//     tempList = {value: arr[k], rest: returnList};
+//     Object.assign(returnList.rest, tempList);
+//     console.log(returnList)
+//   }
+  let returnList = {}
+  Object.assign(returnList.value: arr.shift[0], : arrayToList(arr))
+  return(returnList);
 }
 
 function listToArray(list, arr) {
@@ -9,9 +16,9 @@ function listToArray(list, arr) {
 // tests
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
-// → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
-// → 20
+// console.log(listToArray(arrayToList([10, 20, 30])));
+// // → [10, 20, 30]
+// console.log(prepend(10, prepend(20, null)));
+// // → {value: 10, rest: {value: 20, rest: null}}
+// console.log(nth(arrayToList([10, 20, 30]), 1));
+// // → 20
